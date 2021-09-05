fe5cd6ed26a2bc2bb468d7db6641418681444b45
Umichan <49395863+umichan0621@users.noreply.github.com>
2021-09-05 13:50:21 +0800

    update

--- translate_text.py
+++ translate_text.py
@@ -1,12 +1,9 @@
 from function.machine_translator import TextTranslator
 
-api_id="api_id"
-api_key="api_key"
-text_translator=TextTranslator()
+api_id = "api_id"
+api_key = "api_key"
+text_translator = TextTranslator()
 text_translator.SetExcelPath("dictionary.xlsx")
-text_translator.SetIdAndKey(api_id,api_key)
+text_translator.SetIdAndKey(api_id, api_key)
 text_translator.SetOutputPath("output.txt")
-text_translator.Parse("example.txt","utf-16le")
-
-
-
+text_translator.Parse("example.txt", "utf-16le")
