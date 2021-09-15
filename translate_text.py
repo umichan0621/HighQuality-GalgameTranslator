aa97ef51ec992c33adc693703b57814e4e40ba2f
Umichan <49395863+umichan0621@users.noreply.github.com>
2021-09-15 21:29:41 +0800

    delete

--- translate_text.py
+++
@@ -1,9 +0,0 @@
-from function.general_text_processor import TextTranslator
-
-api_id = "api_id"
-api_key = "api_key"
-text_translator = TextTranslator()
-text_translator.SetExcelPath("dictionary.xlsx")
-text_translator.SetIdAndKey(api_id, api_key)
-text_translator.SetOutputPath("output.txt")
-text_translator.Parse("example.txt", "utf-16le")
