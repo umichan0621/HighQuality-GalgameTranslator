2d3cdb38850c471a255a5b6fddbfa29a3b816154
Umichan <49395863+umichan0621@users.noreply.github.com>
2021-09-16 13:32:24 +0800

    modify

--- plugins/bishop.py
+++ plugins/bishop.py
@@ -1,18 +1,6 @@
 from function.excel_parser import ExcelParser
 from function.general_text_processor import GeneralTextProcessor
-from machine_translator.tencent_translator import TencentMachineTranslator
-import time
-
-excel_parser = ExcelParser()
-excel_parser.SetExcelPath("E:\HighQuality-GalgameTranslator\dictionary.xlsx")
-word_map = excel_parser.GetWordMap()
-general_text_processor = GeneralTextProcessor()
-general_text_processor.SetWordMap(word_map)
-tencent_machine_translator = TencentMachineTranslator()
-
-api_id = "AKIDxfT4wa1o30hjjL9yn10hILr1vc4GDEzI"
-api_key = "sNefQvORi6FryXqC3gphinFFzrLAVNK0"
-tencent_machine_translator.SetIdAndKey(api_id, api_key)
+from machine_translator.general_translator import GeneralTranslator
 
 
 # 针对不同游戏做特殊处理
@@ -53,6 +41,19 @@ class BishopTranslator:
     __output_file = 0  # 输出文件
     __total_line = 0
     __cur_line = 0
+    __general_text_processor = 0
+    __general_machine_translator = 0
+
+    def SetGeneralTrans(self, api_id, api_key):
+        self.__general_machine_translator = GeneralTranslator()
+        self.__general_machine_translator.SetApiIdAndKey(api_id, api_key)
+
+    def SetWordDicPath(self, word_dic_path):
+        excel_parser = ExcelParser()
+        excel_parser.SetExcelPath(word_dic_path)
+        word_map = excel_parser.GetWordMap()
+        self.__general_text_processor = GeneralTextProcessor()
+        self.__general_text_processor.SetWordMap(word_map)
 
     def SetSourceTextPath(self, file_path):
         # 读取原文并保存为数组
@@ -66,38 +67,42 @@ class BishopTranslator:
         # 读取译文文件行数
         self.__cur_line = len(open(target_path, 'r', encoding="utf-16le").readlines())
 
+    def GetCurLine(self):
+        return self.__cur_line
+
+    def GetTotalLine(self):
+        return self.__total_line
+
     def TranslateSourceText(self):
         while self.__cur_line < self.__total_line:
-            self.__HandleText()
-            time.sleep(0.1)
+            self.HandleText()
+            # time.sleep(0.1)
 
-    def __HandleText(self):
+    def HandleText(self):
         source_text = self.__source_file_content[self.__cur_line]
         # 偶数行是标记行
         if self.__cur_line % 2 == 0:
             self.__output_file.write(source_text.replace('0}', '1}'))
+            self.__cur_line += 1
+            return [source_text]
         # 奇数行是原文文本
         else:
-            self.__HandleSourceText(source_text)
-        self.__cur_line += 1
+            res = self.__HandleSourceText(source_text)
+            self.__cur_line += 1
+            return [source_text, res]
 
     def __HandleSourceText(self, source_text):
         # 文本特殊处理
         res = ModifySourceSpecialText(source_text)
         # 文本通用处理
-        res = general_text_processor.ModifySourceText(res)
+        res = self.__general_text_processor.ModifySourceText(res)
         # 机器翻译文本
-        res = tencent_machine_translator.Translate(res)
+        res = self.__general_machine_translator.Translate(res)
         # 复原通用处理
-        res = general_text_processor.RecoverTransText(res)
+        res = self.__general_text_processor.RecoverTransText(res)
         # 复原特殊处理
         res = RecoverTransSpecialText(source_text, res)
-        print(res)
-        # print("----------------------")
         self.__output_file.write(res + '\n')
         # res = res.replace('\n', '')  # 删除文本换行的换行符
+        return res
 
-
-B = BishopTranslator()
-B.SetSourceTextPath("../example.txt")
-B.TranslateSourceText()
