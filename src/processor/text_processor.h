f9778ea00cd6a7f5ff14966fd0ff3102e686b982
Umichan <umichan0621@gmail.com>
2023-01-17 15:18:57 +0800

    Update text_processor.h

--- src/processor/text_processor.h
+++ src/processor/text_processor.h
@@ -1,21 +1,36 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+#include <functional>
 
 namespace gal {
 namespace processor {
 
-using Dict = std::unordered_map<std::string, std::string>;
+using str_func = std::function<std::string()>;
 
 class TextProcessor {
+    using Dict = std::unordered_map<std::string, std::string>;
+
 public:
     TextProcessor();
 
+    std::string Process(const std::string& text);
+
+    std::string Recover(const std::string& text);
+
+    void InsertWord(const std::string& word, const std::string& translation);
+
+    void InsertWord(const std::string& word, const std::string& translation, str_func random_str);
+
 private:
-    Dict src_tmp_dic;
-    Dict tmp_des_dic;
-    Dict des_tmp_dic;
-    std::vector<std::string> src_list;
+    const std::vector<std::string>& word_list();
+
+private:
+    Dict word_tmp_dic_;
+    Dict tmp_trans_dic_;
+    Dict trans_tmp_dic_;
+    std::vector<std::string> word_list_;
+    bool need_sort_;
 };
 
 }  // namespace processor
