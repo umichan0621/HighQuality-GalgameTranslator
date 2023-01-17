6186bac6cefca68757c5821ee7f13ca8bc702e79
Umichan <umichan0621@gmail.com>
2023-01-17 18:49:12 +0800

    Create file.hpp

---
+++ src/utils/file.hpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <string>
+#include <vector>
+
+namespace gal {
+namespace utils {
+
+static bool ReadFile(const std::string& path, std::vector<std::string>* content) {
+    std::fstream fs;
+    fs.open(path, std::ios::in);
+    if (!fs.is_open()) {
+        return false;
+    }
+    std::string temp;
+
+    while (getline(fs, temp)) {
+        content->emplace_back(temp);
+    }
+    fs.close();
+    return true;
+}
+
+}  // namespace utils
+}  // namespace gal
