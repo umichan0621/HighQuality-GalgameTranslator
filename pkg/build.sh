2e22c90ca65ea150de9a2efa047bcd2976af4b40
gongjie <gongjie@autowise.ai>
2023-01-18 16:21:49 +0800

    detach golang interface

--- pkg/build.sh
+++
@@ -1,8 +0,0 @@
-go build -o libGoInterface.so -buildmode=c-shared cmd/main.go
-
-DES_PATH=../third_party/
-mkdir -p ${DES_PATH}/include
-mkdir -p ${DES_PATH}/lib
-
-mv libGoInterface.h ${DES_PATH}/include/GoInterface.h
-mv libGoInterface.so ${DES_PATH}/lib/libGoInterface.so
--- pkg/go.mod
+++
@@ -1,7 +0,0 @@
-module main
-
-go 1.19
-
-require github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/tmt v1.0.579
-
-require github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.579 // indirect
