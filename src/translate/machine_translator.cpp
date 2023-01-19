961f90daf4125f155e47b7b17c7a0e439e4b9e11
umichan0621 <umichan0621@gmail.com>
2023-01-19 16:54:07 +0800

    update trans

--- src/translate/machine_translator.cpp
+++ src/translate/machine_translator.cpp
@@ -1,6 +1,8 @@
 #include "machine_translator.h"
 
+#include <cmath>
 #include <iostream>
+#include <thread>
 #include <yaml-cpp/yaml.h>
 
 #include "tencent_trans.h"
@@ -11,7 +13,8 @@ using std::endl;
 namespace gal {
 namespace trans {
 
-MachineTranslator::MachineTranslator() {}
+MachineTranslator::MachineTranslator()
+    : last_translate_time_(std::chrono::high_resolution_clock::now()), sleep_time_(1000) {}
 
 void MachineTranslator::LoadConfig(const std::string& config_path) {
     YAML::Node machine_translator = YAML::LoadFile(config_path);
@@ -26,10 +29,15 @@ void MachineTranslator::LoadConfig(const std::string& config_path) {
             std::string secret_id = translator["secret_id"].as<std::string>();
             std::string secret_key = translator["secret_key"].as<std::string>();
             double trans_freq = translator["trans_freq"].as<double>();
+
             tencent_trans->Init(secret_id, secret_key);
             tencent_trans->SetRegion();
             tencent_trans->SetLanguage("zh", "jp");
             translator_map_["tencent"] = tencent_trans;
+
+            // trans_freq = n -> n trans/sec
+            int32_t sleep_time = ceil(1000.0 / trans_freq);
+            sleep_time_ = std::chrono::milliseconds(sleep_time);
         }
         // else if
         // else if
@@ -48,7 +56,23 @@ std::string MachineTranslator::Translate(const std::string& src_text) {
     if (!cur_translator_) {
         return "error";
     }
-    return cur_translator_->Translate(src_text);
+
+    std::this_thread::sleep_until(last_translate_time_ + sleep_time_);
+    std::string res = cur_translator_->Translate(src_text);
+    last_translate_time_ = std::chrono::high_resolution_clock::now();
+    return res;
+}
+
+std::string MachineTranslator::Translate(const std::vector<utils::AnalyzerRes>& analyzer_res) {
+    std::string res = "";
+    for (const auto& sub_res : analyzer_res) {
+        if (!sub_res.need_tranlate) {
+            res += sub_res.text;
+        } else {
+            res += Translate(sub_res.text);
+        }
+    }
+    return res;
 }
 
 }  // namespace trans
--- src/translate/machine_translator.h
+++ src/translate/machine_translator.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <chrono>
+#include <vector>
 #include <unordered_map>
+
+#include <utils/analyzer_res.h>
 
 #include "trans_interface.h"
 
@@ -17,8 +21,11 @@ public:
 
     std::string Translate(const std::string& src_text);
 
+    std::string Translate(const std::vector<utils::AnalyzerRes>& analyzer_res);
+
 private:
-    int32_t trans_freq_;
+    std::chrono::high_resolution_clock::time_point last_translate_time_;
+    std::chrono::milliseconds sleep_time_;
     TransInterface* cur_translator_;
     std::unordered_map<std::string, TransInterface*> translator_map_;
 };
